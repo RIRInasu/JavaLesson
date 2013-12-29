e1fc61ca183d6876662f45ecc032dabcb3f90498
ririnasu <chris06@live.jp>
2013-12-29 22:02:28 +0900

    nyasu

--- Java/src/Java8_2.java
+++ Java/src/Java8_2.java
@@ -1,21 +1,17 @@
 public class Java8_2 {
 	public static void main(String[] args) {
-		calcGraph(8);
+		printArc(8);
 	}
 
 	public static void printGraph(int x) {
 		for (int i = 0; i < x; i++) {
 			System.out.print("*");
-
 		}
 		System.out.println("");
 	}
 
-	public static void calcGraph(int x) {
-		for (int i = x; 0 < i; i--) {
-			printGraph(i * i);
-		}
-		for (int i = 0; i <= x; i++) {
+	public static void printArc(int x) {
+		for (int i = -x; i <= x; i++) {
 			printGraph(i * i);
 		}
 	}
